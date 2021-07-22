53251a5f349a8fd70c4e60da892f7e7bedf5b2d1
ruben-xe <rubenrodriguezxp@gmail.com>
2021-07-22 15:03:19 -0300

    4 files modified, 6 added, 1 deleted

---
+++ src/containers/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FaceCanvas from '../components/FaceCanvas';
+import Header from '../components/Header';
+const Home = () => (
+  <>
+    <Header />
+    <FaceCanvas />;
+  </>
+);
+export default Home;
